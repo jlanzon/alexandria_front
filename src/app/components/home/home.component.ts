2729cbd6472bc260a6244c9696da4c0b28204ec8
Joe Lanzon <joelanzon@outlook.com>
2021-07-13 15:16:17 +0100

    ui changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+
 })
 export class HomeComponent implements OnInit {
 
-  readonly ROOT_URL= 'https://digital.nhs.uk/restapi/CyberAlert'
+  // readonly ROOT_URL= 'https://digital.nhs.uk/restapi/CyberAlert'
+  ROOT_URL= 'https://digital.nhs.uk/restapi/CyberAlert/?_limited=true'
   posts: any;
 
   constructor(private http: HttpClient) { }
@@ -16,7 +19,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {}
 
   getPosts(){
-    this.posts = this.http.get(this.ROOT_URL + '/page')
+    this.posts = this.http.get(this.ROOT_URL)
   }
 
 }
