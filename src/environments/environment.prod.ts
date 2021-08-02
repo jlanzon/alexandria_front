cd610eee19046a30e9ff4c74cd19f15593793192
Joseph Lanzon <joelanzon@outlook.com>
2021-08-02 11:48:44 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
+  firebase: {
+    apiKey: "AIzaSyDltHIjMJUlu6fuK6_utmXKKQOm1deg4mw",
+    authDomain: "alexandria-e94eb.firebaseapp.com",
+    projectId: "alexandria-e94eb",
+    storageBucket: "alexandria-e94eb.appspot.com",
+    messagingSenderId: "365905800719",
+    appId: "1:365905800719:web:4beffc20c7cf12a1d3bd7a",
+    measurementId: "G-7GBB2S52GB"
+  },
 };
