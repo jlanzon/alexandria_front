ff0afb9bfeaedaf7e9cb50eb6438a0cd2b261ff4
Joe Lanzon <joelanzon@outlook.com>
2021-08-12 17:27:23 +0100

    admindash & doc changes

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, timer } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { interval, Observable, timer } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { documents } from 'src/app/services/document.model';
 
 @Component({
   selector: 'app-search',
@@ -9,13 +13,18 @@ import { interval, timer } from 'rxjs';
 export class SearchComponent implements OnInit {
 
   document = "Publications"
+  user = AuthService
+  documentCollection: AngularFirestoreCollection<documents>
+  documents: Observable<any[]>
+
 
   pdfSrc: string = '../../../assets/jsp834.png';
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth, public auth: AuthService, public afs: AngularFirestore) { }
 
   ngOnInit(): void {
-
+    this.documentCollection = this.afs.collection('documents')
+    this.documents = this.documentCollection.valueChanges()
   }
 
 }
