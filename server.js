32897ff679fddf7f93db8ed4eae11040508ae028
Joseph Lanzon <joelanzon@outlook.com>
2021-11-08 10:57:55 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,5 +15,9 @@ app.get('*', (req, res) => {
     res.sendFile(`/dist/alexandria/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
+app.get('*', function(req, res){
+  res.status(404).send('what???');
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
